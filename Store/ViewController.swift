246aeb0f3c9c6559f91f007ee2aceaef866e9120
Marcin Karski <karski80@gmail.com>
2019-03-08 09:58:00 +0000

    Initial Commit

---
+++ Store/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        view.backgroundColor = .white
+        self.title = "Store"
+    }
+}
